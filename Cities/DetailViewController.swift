742a20077c5c2a1a95059dc7ce4be7cca5ff3639
Mosahaq <Mosahaq@gmail.com>
2020-01-14 10:56:51 -0600

    Connected UI objects to storyboard

--- Cities/DetailViewController.swift
+++ Cities/DetailViewController.swift
@@ -8,33 +8,52 @@
 
 import UIKit
 
-class DetailViewController: UIViewController {
-
-    @IBOutlet weak var detailDescriptionLabel: UILabel!
-
-
-    func configureView() {
-        // Update the user interface for the detail item.
-        if let detail = detailItem {
-            if let label = detailDescriptionLabel {
-                label.text = detail.description
-            }
-        }
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        configureView()
-    }
-
-    var detailItem: NSDate? {
-        didSet {
-            // Update the view.
+class DetailViewController: UIViewController
+{
+    @IBOutlet weak var cityTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var populationTextField: UITextField!
+    @IBOutlet var imageView: UIView!
+    
+    var detailItem: City?
+    {
+        didSet
+        {
             configureView()
         }
     }
 
+    func configureView()
+    {
+       // Update the user interface for the detail item
+       if let city = self.detailItem
+       {
+          if cityTextField != nil
+          {
+             cityTextField.text = city.name
+             stateTextField.text = city.state
+             populationTextField.text = String(city.population)
+             imageView.image = UIImage(data: city.image)
+          }
+       }
+    }
+    
+    func viewWillDissapear()
+    {
+        if let city = self.detailItem
+        {
+           city.name = cityTextField.text!
+           city.state = stateTextField.text!
+           city.population = Int(populationTextField.text!)!
+        }
+    }
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
 
 }
 
