a58768fd676c5423d6995978cd786302eb20d433
Mosahaq <Mosahaq@gmail.com>
2020-01-16 10:39:07 -0600

    Added Alert View

--- Cities/MasterViewController.swift
+++ Cities/MasterViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class MasterViewController: UITableViewController {
+class MasterViewController: UITableViewController
+{
 
     var detailViewController: DetailViewController? = nil
-    var objects = [Any]()
+    var cities = [City]()
 
 
     override func viewDidLoad() {
@@ -27,23 +28,60 @@ class MasterViewController: UITableViewController {
         }
     }
 
-    override func viewWillAppear(_ animated: Bool) {
+    override func viewWillAppear(_ animated: Bool)
+    {
         clearsSelectionOnViewWillAppear = splitViewController!.isCollapsed
         super.viewWillAppear(animated)
     }
 
     @objc
-    func insertNewObject(_ sender: Any) {
-        objects.insert(NSDate(), at: 0)
-        let indexPath = IndexPath(row: 0, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
+    func insertNewObject(_ sender: Any)
+    {
+        let alert = UIAlertController(title: "Add City", message: nil, preferredStyle: .alert)
+        
+        alert.addTextField { (textField) in
+           textField.placeholder = "City"
+        }
+        alert.addTextField { (textField) in
+           textField.placeholder = "State"
+        }
+        alert.addTextField { (textField) in
+           textField.placeholder = "Population"
+           textField.keyboardType = .numberPad
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(cancelAction)
+        
+        let insertAction = UIAlertAction(title: "Add", style: .default) { (action) in
+                   let cityTextField = alert.textFields![0] as UITextField
+                   let stateTextField = alert.textFields![1] as UITextField
+                   let populationTextField = alert.textFields![2] as UITextField
+                   guard let image = UIImage(named: cityTextField.text!) else{
+                      print("missing \(cityTextField.text!) image")
+                      return }
+                   if let population = Int(populationTextField.text!) {
+                      let city = City(name: cityTextField.text!,
+                                    state: stateTextField.text!,
+                                population: population,
+                                    image: image.pngData()!)
+                      self.cities.append(city)
+                      self.tableView.reloadData()
+                   }
+        }
+        
+        alert.addAction(insertAction)
+        present(alert, animated: true, completion: nil)
     }
 
     // MARK: - Segues
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showDetail" {
-            if let indexPath = tableView.indexPathForSelectedRow {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if segue.identifier == "showDetail"
+        {
+            if let indexPath = tableView.indexPathForSelectedRow
+            {
                 let object = objects[indexPath.row] as! NSDate
                 let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
                 controller.detailItem = object
@@ -56,28 +94,34 @@ class MasterViewController: UITableViewController {
 
     // MARK: - Table View
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
+    override func numberOfSections(in tableView: UITableView) -> Int
+    {
         return 1
     }
 
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
         return objects.count
     }
 
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let object = objects[indexPath.row] as! NSDate
         cell.textLabel!.text = object.description
         return cell
     }
 
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool
+    {
         // Return false if you do not want the specified item to be editable.
         return true
     }
 
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+    {
+        if editingStyle == .delete
+        {
             objects.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
